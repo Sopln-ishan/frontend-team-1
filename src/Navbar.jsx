7612aff38a252b7b155302eb75c15f924972b471
Sopln-ishan <23ishansekhar10d@gmail.com>
2025-03-10 20:21:37 +0530

    added media query to navbar and to home page, bug - the home page background image

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,6 +3,34 @@ const Navbar = () => {
     return (
         <>
             <h1 className='logo'>Logo</h1>
+            <div className='dropdown-nav-bar' style={{ display: 'none' }}>
+                <div className='dropdown-button'>
+                    &#x2630;
+                </div>
+
+                <div className='dropdown-content'>
+                <Link to="/">
+                    Home
+                </Link>
+
+                <Link to="/About-Us.jsx">
+                    About Us 
+                </Link>
+
+                <Link to="/Why-Us.jsx">
+                    Why Us 
+                </Link>
+
+                <Link to="/Our-Services.jsx">
+                    Our Services 
+                </Link>
+
+                <Link to="/Contact.jsx">
+                    Contact Us 
+                </Link>    
+                </div>
+            </div>
+
             <div className="navigation-menu">
                 <Link to="/">
                     Home
