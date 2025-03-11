d857356fc37361b86827a8c451477e5556abb81f
[Sarthak Yadav] <[yadavsarthak01.24@kgpian.iitkgp.ac.in]>
2025-03-11 22:32:31 +0530

    added Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,42 @@
+import React from 'react';
+import './Footer.css';
+
 const Footer = () => {
-    return (
-        <>
-            <button>Footer</button>
-        </>
-    );
-}
- 
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <h2 className="footer-name">COMPANY NAME</h2>
+          <p className="footer-copyright">All Rights Reserved, 2025</p>
+        </div>
+        
+        <div className="footer-middle">
+          <nav className="footer-nav">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/blogs">Our Services</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+        
+        <div className="footer-right">
+          <div className="footer-social">
+            <p>Get to know us</p>
+            <div className="social-icons">
+              <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
+                <i className="linkedin-icon">in</i>
+              </a>
+              <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                <i className="twitter-icon">X</i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
