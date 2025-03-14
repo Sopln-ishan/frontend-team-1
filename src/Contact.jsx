11e105fba3805f5ab7ceed4a2acf2ef027133390
[Sarthak Yadav] <[yadavsarthak01.24@kgpian.iitkgp.ac.in]>
2025-03-14 16:05:41 +0530

    changes the background images and contact us country-cards

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -14,7 +14,7 @@ const CountryCard = ({ country }) => {
       {/* <h4 className="country-name">{country.name}</h4> */}
       <div className="extraimg">  
         <h4 className="country-name">{country.name}</h4>
-        <img src={country.image1} alt={`${country.name} Image`} className="country-image" />
+        {/* <img src={country.image1} alt={`${country.name} Image`} className="country-image" /> */}
       </div>
       <p className="country-description">{country.description}</p>
     </div>
@@ -35,67 +35,67 @@ const Contact = () => {
       name: "UAE",
       image1: "https://images.trvl-media.com/lodging/3000000/2240000/2235400/2235336/88658e84.jpg?impolicy=resizecrop&rw=575&rh=575&ra=fill",
       image2: "https://images.trvl-media.com/lodging/3000000/2240000/2235400/2235336/88658e84.jpg?impolicy=resizecrop&rw=575&rh=575&ra=fill",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "Saudi Arabia",
       image1: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/11/85/f3/b7.jpg",
       image2: "https://media-cdn.tripadvisor.com/media/attractions-splice-spp-674x446/11/85/f3/b7.jpg",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "OMAN",
       image1: "https://www.holidify.com/images/cmsuploads/compressed/shutterstock_80147977_20191202163915.jpg",
       image2: "https://www.holidify.com/images/cmsuploads/compressed/shutterstock_80147977_20191202163915.jpg",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "BAHRAIN",
       image1: "https://live.staticflickr.com/4100/4884934423_125d9bca35_b.jpg",
       image2: "https://m.media-amazon.com/images/I/61ywLsK+idL._AC_UF1000,1000_QL80_.jpg",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "KUWAIT",
       image1: "https://i0.wp.com/nenow.in/wp-content/uploads/2024/12/kuwait-1.jpeg?resize=780%2C585&quality=89&ssl=1",
       image2: "https://i0.wp.com/nenow.in/wp-content/uploads/2024/12/kuwait-1.jpeg?resize=780%2C585&quality=89&ssl=1",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "QATAR",
       image2: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/29/ed/64/28/lusail-promenade.jpg?w=500&h=500&s=1",
       image1: "https://llqlifestyle.com/wp-content/uploads/2022/05/15a496b0-6c7d-4bd4-a2ad-ae269fe21c95-1024x756.jpg",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "JORDAN",
       image1: "https://www.planetware.com/wpimages/2020/03/jordan-top-attractions-petra.jpg",
       image2: "https://www.planetware.com/wpimages/2020/03/jordan-top-attractions-petra.jpg",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "IRAQ",
       image1: "https://media.istockphoto.com/id/1441414728/photo/agha-bozorg-mosque-kashan-iraq.jpg?s=612x612&w=0&k=20&c=zJ0yagPZHwQZcvqByBQAHXPmphTOPbyILuTW7jJeX_4=",
       image2: "https://media.istockphoto.com/id/1441414728/photo/agha-bozorg-mosque-kashan-iraq.jpg?s=612x612&w=0&k=20&c=zJ0yagPZHwQZcvqByBQAHXPmphTOPbyILuTW7jJeX_4=",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "SYRIA",
       image1: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/eb/32/93/an-amazing-place-to-visit.jpg?w=500&h=500&s=1",
       image2: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/eb/32/93/an-amazing-place-to-visit.jpg?w=500&h=500&s=1",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "LEBANON",
       image1: "https://media.istockphoto.com/id/513441442/photo/sea-landscape-with-pigeons-rock-beirut-lebanon.jpg?s=612x612&w=0&k=20&c=Ay_boBxPPx86n_hC78m8S9swFKIROjKQLbbRfXSEBh8=",
       image2: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/49/49/81/caption.jpg?w=500&h=500&s=1",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "EGYPT",
       image1: "https://media.istockphoto.com/id/506604108/photo/full-sphinx-profile-pyramids-walkway-giza.jpg?s=612x612&w=0&k=20&c=DvpXHu0OvQjKLzSwslZ3iCScIMiebWmOPxxGBV0yLyU=",
       image2: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/06/7e/7d/2c/pyramids-of-giza.jpg?w=500&h=500&s=1",
-      // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
   ];
 
@@ -155,6 +155,7 @@ const Contact = () => {
             ))}
           </div>
         </section>
+        <section className="query">
         <div className="form-container">
           <h2>Raise a Query</h2>
           {submitted ? (
@@ -182,6 +183,7 @@ const Contact = () => {
             </form>
           )}
         </div>
+        </section>
       </div>
     </>
   );
