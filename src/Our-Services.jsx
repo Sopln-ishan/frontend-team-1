3acc6aacc3544a70ac77f2becc06f346b5605df2
Sopln-ishan <23ishansekhar10d@gmail.com>
2025-03-18 19:53:09 +0530

    transition time for our services reduced

--- src/Our-Services.jsx
+++ src/Our-Services.jsx
@@ -63,7 +63,7 @@ const OurServices = () => {
             initial='hidden'
             whileInView='visible'
             viewport={{ once: true }}
-            transition={{ duration: 0.5, delay: 0.25*linkCard.id }}
+            transition={{ duration: 0.5, delay: 0.25 }}
             key={ linkCard.id }
           >
             <Link to={ linkCard.src } className='link-card'>
